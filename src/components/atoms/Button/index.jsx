2a1b55afecbe4071c3eb2084d71214474d87c742
valholic <yansenlodova@gmail.com>
2025-09-12 22:57:46 +0800

    height button

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,5 +1,5 @@
 export default function Button({ type, name, handleClick, width}) {
     return (
-        <button type={type} onClick={handleClick} className={`bg-[#d4af37] rounded-md ${width} p-2 h-10 lg:h-12 text-black lg:text-2xl font-mono font-semibold hover:bg-[#d4af37]/90 cursor-pointer`}>{ name }</button>
+        <button type={type} onClick={handleClick} className={`bg-[#d4af37] rounded-md ${width} p-2 h-fit text-black lg:text-2xl font-mono font-semibold hover:bg-[#d4af37]/90 cursor-pointer`}>{ name }</button>
     )
 }
