28b90f59c406af77eb5ad5846bc3399b137e4d21
valholic <yansenlodova@gmail.com>
2025-09-13 17:00:47 +0800

    gtw

--- src/components/molecules/HistoryCard/index.jsx
+++ src/components/molecules/HistoryCard/index.jsx
@@ -17,9 +17,9 @@ export default function HistoryCard({ time, pName, amount, price, gId, uId, uNam
     return (
         <div className="flex flex-col md:grid md:grid-cols-3 gap-2 p-4 bg-[#353535] rounded-lg w-full h-fit relative">
             {isReview && <ReviewForm handleOut={() => setIsReview(false)} gId={gId} uId={uId} uName={uName} />}
-            <Ellipsis className="text-[#d4af37] text-3xl absolute right-2 top-1 cursor-pointer" onClick={() => setIsClick(isClick ?false : true)} />
+            <Ellipsis className="text-[#d4af37] text-3xl absolute right-2 top-1 cursor-pointer" onClick={() => setIsClick(isClick ? false : true)} />
             <div className={`${isClick ? "flex" : "hidden"} flex-col w-fit h-fit absolute right-3 top-7 font-bold`}>
-                <p className="text-black text-xl cursor-pointer py-1 px-4 bg-[#d4af37] hover:bg-[#d4af37]/90 rounded-t-md" onClick={() => setIsReview(true)}>Review</p>
+                <p className="text-black text-xl cursor-pointer py-1 px-4 bg-[#d4af37] hover:bg-[#d4af37]/90 rounded-md" onClick={() => setIsReview(true)}>Review</p>
             </div>
             <div className="flex flex-col gap-y-2 font-bold">
                 <Link to={type === 'product' ? `/product/detail/${gId}/${toSlug(pName)}` : `/booking/detail/${gId}/${toSlug(pName)}`} className="text-2xl text-[#d4af37]">{pName}</Link>
--- src/components/molecules/ReviewForm/index.jsx
+++ src/components/molecules/ReviewForm/index.jsx
@@ -1,13 +1,18 @@
 import { X } from "lucide-react";
 import { Button, InputFile, Textarea } from "../../atoms";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { TbStar, TbStarFilled } from "react-icons/tb";
+import ConfirmAlert from "../ConfirmAlert";
 
 export default function ReviewForm({ handleOut, uName, uId, gId }) {
     const [comment, setComment] = useState('');
     const [rating, setRating] = useState(0);
     const [img, setImg] = useState(null);
+    const [hasReviewed, setHasReviewed] = useState(false);
+    const [isDelete, setIsDelete] = useState(false);
+    const [isFinished, setIsFinished] = useState(false);
+    const [rId, setRId] = useState('');
     const star = [1, 2, 3, 4, 5];
 
     function OnUploadImage(e) {
@@ -15,7 +20,7 @@ export default function ReviewForm({ handleOut, uName, uId, gId }) {
         setImg(uploadedImage);
     }
 
-    function OnAddReview(e) {
+    function OnSubmit(e) {
         e.preventDefault();
 
         const data = new FormData();
@@ -33,9 +38,38 @@ export default function ReviewForm({ handleOut, uName, uId, gId }) {
         .then(handleOut);
     }
 
+    function OnDeleteReview() {
+        axios.delete(`https://valbershop-api.vercel.app/v1/shop/review/delete/${gId}/${rId}`)
+        .then(handleOut);
+    }
+
+    useEffect(() => {
+        axios.get(`https://valbershop-api.vercel.app/v1/shop/get/${gId}`)
+        .then(result => {
+            result.data.goodsData.review.forEach(rev => {
+                if(rev.user_id === uId) {
+                    console.log(rev)
+                    setHasReviewed(true);
+                    setComment(rev.comment);
+                    setRating(rev.star);
+                    if(rev.review_img) {
+                        setImg(rev.review_img);
+                    }
+                    setRId(rev._id);
+                }
+            })
+            setIsFinished(true);
+        })
+    }, [gId, uId]);
+
     return (
         <div className="fixed inset-0 bg-black/70 z-30 w-screen h-screen flex flex-col items-center justify-center p-10">
-            <form className="w-[90%] md:w-[60%] h-fit absolute flex flex-col gap-y-4 z-70 bg-[#363636] p-5 rounded-2xl font-Poiret" onSubmit={OnAddReview}>
+            <form className="w-[90%] md:w-[60%] h-fit absolute flex flex-col gap-y-4 z-70 bg-[#363636] p-5 rounded-2xl font-Poiret" onSubmit={OnSubmit}>
+            {isDelete && 
+                <ConfirmAlert title={'Confirm this action'} text={'Are you sure to delete your review?'} handleNo={() => {
+                    setIsDelete(false);
+                }} handleYes={OnDeleteReview} />
+            }
                 <X className="w-8 h-8 absolute top-2 right-2 text-white cursor-pointer" onClick={handleOut} />
                 <Textarea name={'comment'} label={'Comment'} value={comment} handleChange={e => setComment(e.target.value)} required={true} />
                 <p className="text-2xl text-[#d4af37] font-bold">Rate the product</p>
@@ -48,8 +82,17 @@ export default function ReviewForm({ handleOut, uName, uId, gId }) {
                         }
                     })}
                 </div>
-                <InputFile name={'review-img'} label={'You can also add image!'} required={false} handleChange={e => OnUploadImage(e)} />
-                <Button type={'submit'} name={'Add review'} />
+                {img !== null && 
+                    <div className="flex flex-col w-fit h-fit gap-y-4">
+                        <img src={`https://valbershop-api.vercel.app/${img}`} className="w-1/5 h-1/4 rounded-2xl" loading="lazy" />
+                        <Button type={'button'} name={'Delete image'} handleClick={() => setImg(null)} width={'w-1/5'} />
+                    </div>
+                }
+                {img === null && 
+                    <InputFile name={'review-img'} label={'You can also add image!'} required={false} handleChange={e => OnUploadImage(e)} />
+                }
+                {isFinished && !hasReviewed && <Button type={'submit'} name={'Add review'} />}
+                {isFinished && hasReviewed && <Button type={'button'} name={'Delete review'} handleClick={() => setIsDelete(true)} />}
             </form>
         </div>
     )
