dbb70dc6c2958309b30ba29212baf89ad453d882
valholic <yansenlodova@gmail.com>
2025-09-13 20:11:09 +0800

    admin access

--- src/components/molecules/ConfirmAlert/index.jsx
+++ src/components/molecules/ConfirmAlert/index.jsx
@@ -3,7 +3,7 @@ import { Button } from "../../atoms"
 export default function ConfirmAlert({ title, text, handleYes, handleNo}) {
     return (
         <div className="fixed inset-0 bg-black/70 z-60 flex items-center justify-center font-Poiret">
-            <div className="w-[80%] md:w-[30%] h-[50%] md:h-[30%] absolute flex flex-col gap-y-10 z-70 bg-[#353535] text-white p-5 rounded-2xl">
+            <div className="w-[80%] md:w-[30%] h-fit absolute flex flex-col gap-y-10 z-70 bg-[#353535] text-white p-5 rounded-2xl">
                 <p className="text-4xl text-center w-full font-bold">{ title }</p>
                 <p className="text-2xl text-center w-full font-semibold">{ text }</p>
                 <div className="flex gap-x-3 w-full justify-center">
--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,26 +1,47 @@
-import { Link, Route, Routes, useLocation } from "react-router-dom";
+import { Link, Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { Dashboard, GalleryForm, ProductForm, TestimonyForm } from "../index";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Admin() {
     const location = useLocation();
+    const navigate = useNavigate();
     const path = location.pathname;
+    const token = localStorage.getItem('token');
+    const [isFinished, setIsFinished] = useState(false);
 
-    return (
-        <div className="flex flex-grow font-Poiret bg-[#0c0c0c] flex-col lg:flex-row p-5 gap-5">
-            <section className="flex text-center lg:text-start text-white lg:text-black w-full lg:w-1/5 bg-transparent lg:bg-[#d4af37] lg:border-r lg:border-white text-base md:text-xl lg:text-3xl md:p-1 lg:p-5 font-semibold flex-row lg:flex-col gap-x-2 lg:gap-y-10">
-                <Link to={'/admin/dashboard'} className={`p-2 w-full rounded-2xl border border-[#d4af37] hover:bg-[#d4af37] lg:hover:bg-black/20 ${path === '/admin/dashboard' ? "bg-[#d4af37] lg:bg-black/20" : ""}`}>Dashboard</Link>
-                <Link to={'/admin/product'} className={`p-2 w-full rounded-2xl border border-[#d4af37] hover:bg-[#d4af37] lg:hover:bg-black/20 ${path === '/admin/product' ? "bg-[#d4af37] lg:bg-black/20" : ""}`}>Products & Services</Link>
-                <Link to={'/admin/gallery'} className={`p-2 w-full rounded-2xl border border-[#d4af37] hover:bg-[#d4af37] lg:hover:bg-black/20 ${path === '/admin/gallery' ? "bg-[#d4af37] lg:bg-black/20" : ""}`}>Gallery</Link>
-                <Link to={'/admin/testimony'} className={`p-2 w-full rounded-2xl border border-[#d4af37] hover:bg-[#d4af37] lg:hover:bg-black/20 ${path === '/admin/testimony' ? "bg-[#d4af37]lg:bg-black/20" : ""}`}>Testimony</Link>
-            </section>
-            <section className="flex flex-col w-full">
-                <Routes>
-                    <Route path="/dashboard" Component={Dashboard} />
-                    <Route path="/product" Component={ProductForm} />
-                    <Route path="/gallery" Component={GalleryForm} />
-                    <Route path="/testimony" Component={TestimonyForm} />
-                </Routes>
-            </section>
-        </div>
-    )
+    useEffect(() => {
+        axios.get(`https://valbershop-api.vercel.app/v1/api/me`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(result => {
+            if(result.data.user.role.toLowerCase() !== 'admin') {
+                navigate('/');
+            }
+            setIsFinished(true);
+        });
+    }, [navigate, token])
+
+    if(isFinished) {
+        return (
+            <div className="flex flex-grow font-Poiret bg-[#0c0c0c] flex-col lg:flex-row p-5 gap-5">
+                <section className="flex text-center lg:text-start text-white lg:text-black w-full lg:w-1/5 bg-transparent lg:bg-[#d4af37] lg:border-r lg:border-white text-base md:text-xl lg:text-3xl md:p-1 lg:p-5 font-semibold flex-row lg:flex-col gap-x-2 lg:gap-y-10">
+                    <Link to={'/admin/dashboard'} className={`p-2 w-full rounded-2xl border border-[#d4af37] hover:bg-[#d4af37] lg:hover:bg-black/20 ${path === '/admin/dashboard' ? "bg-[#d4af37] lg:bg-black/20" : ""}`}>Dashboard</Link>
+                    <Link to={'/admin/product'} className={`p-2 w-full rounded-2xl border border-[#d4af37] hover:bg-[#d4af37] lg:hover:bg-black/20 ${path === '/admin/product' ? "bg-[#d4af37] lg:bg-black/20" : ""}`}>Products & Services</Link>
+                    <Link to={'/admin/gallery'} className={`p-2 w-full rounded-2xl border border-[#d4af37] hover:bg-[#d4af37] lg:hover:bg-black/20 ${path === '/admin/gallery' ? "bg-[#d4af37] lg:bg-black/20" : ""}`}>Gallery</Link>
+                    <Link to={'/admin/testimony'} className={`p-2 w-full rounded-2xl border border-[#d4af37] hover:bg-[#d4af37] lg:hover:bg-black/20 ${path === '/admin/testimony' ? "bg-[#d4af37]lg:bg-black/20" : ""}`}>Testimony</Link>
+                </section>
+                <section className="flex flex-col w-full">
+                    <Routes>
+                        <Route path="/dashboard" Component={Dashboard} />
+                        <Route path="/product" Component={ProductForm} />
+                        <Route path="/gallery" Component={GalleryForm} />
+                        <Route path="/testimony" Component={TestimonyForm} />
+                    </Routes>
+                </section>
+            </div>
+        )
+    }
 }
